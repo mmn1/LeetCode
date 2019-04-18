5a7d6eb73289a59078913ffb845bbc97d33f2296
AWGN <guo_xiong_feng@163.com>
2019-04-18 20:13:09 +0800

    测试ssh连接

---
+++ src/lRUCache/Solution.java
@@ -0,0 +1,23 @@
+package lRUCache;
+
+public class Solution {
+	private int capacity;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+        
+    }
+    
+    public void put(int key, int value) {
+        
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
